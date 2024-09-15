9bf065ddca191f470df0c2e6bd10a9f3e2562b07
LimunBF <111760661+LimunBF@users.noreply.github.com>
2024-09-15 09:19:00 +0700

    firstCommit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="About">
+    <h1>
+        Ini Merupakan Halaman ABOUT dari Web Laravel Menggunakan Herd
+    </h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Contact">
+    <h1>
+        Ini Merupakan Halaman CONTACT dari Web Laravel Menggunakan Herd
+    </h1>
+</x-layout>
