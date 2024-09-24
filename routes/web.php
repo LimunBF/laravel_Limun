6a946e969467875edc859f90d3f907aeb8a901da
LimunBF <111760661+LimunBF@users.noreply.github.com>
2024-09-24 10:49:13 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,23 @@ $pekerjaan = [
 
 class Pekerjaan{
     public static function daftar_kerja(){
-        
+        return [
+            [
+                'id' => '1',
+                'judul' => 'Guru',
+                'gaji' => 'Rp. 17.000.000'
+            ],
+            [
+                'id' => '2',
+                'judul' => 'Desainer',
+                'gaji' => 'Rp. 9.000.000'
+            ],
+            [
+                'id' => '3',
+                'judul' => 'Animator',
+                'gaji' => 'Rp. 15.000.000'
+            ]
+        ];
     }
 }
 
